a1c3fc7faf0c8e3181168a8826ec12487ecdd6ac
Meagan Jacobs <megpjacobs@gmail.com>
2022-04-01 21:54:57 -0700

    animated gradient test working

---
+++ crambles/src/Components/GradientTest/GradientTest.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import Granim from "granim";
+import "./GradientTest.css"
+
+const GradientTest =() => {
+
+      useEffect(() => {
+        new Granim({
+          element: "#logo-canvas",
+          direction: "top-bottom",
+          opacity: [1, 1],
+          states: {
+            "default-state": {
+              gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+              ],
+              transitionSpeed: 6000,
+            },
+          },
+        });
+      }, []);
+    
+      return (
+        <div className="bloc-logo">
+          <canvas id="logo-canvas" />
+          <div className="logo-mask"></div>
+        </div>
+      );
+    };
+    
+    
+    export default GradientTest;
--- crambles/src/Components/Level3/Level3.js
+++ crambles/src/Components/Level3/Level3.js
@@ -1,61 +1,80 @@
-
 import React, { useState, useEffect } from "react";
-import './Level3.css'
+import "./Level3.css";
 import { Link } from "react-router-dom";
 import { InputGroup } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { FormControl } from "react-bootstrap";
 
-
 const Level3 = () => {
-//   const randomIndex = Math.floor(Math.random() * WordList.length); //this is the number in the array
+  //   const randomIndex = Math.floor(Math.random() * WordList.length); //this is the number in the array
 
   const [guess, setGuess] = useState("");
   const [feedback, setFeedback] = useState("");
   const [button1, setButton1] = useState("");
   const [button2, setButton2] = useState("");
-  const [box1, setBox1] = useState(false)
-  const [box2, setBox2] = useState(false)
-  const [box3, setBox3] = useState(false)
-//   const [randomNumber, setRandomNumber] = useState(randomIndex);
+  const [box1, setBox1] = useState(false);
+  const [box2, setBox2] = useState(false);
+  const [box3, setBox3] = useState(false);
+  const [box4, setBox4] = useState(false);
+  const [box5, setBox5] = useState(false);
+  const [box6, setBox6] = useState(false);
 
-//   const CheckAnswer = (event) => {
-//     event.preventDefault()
-//     if (WordList[randomNumber].answer === guess) {
-//       setFeedback("you got it right!");
-//       setButton2("go to level 2");
-//       setButton1("");
-//     } else {
-//       setFeedback("nope");
-//       setButton1("Try Again");
-//     }
-//   };
-//   const tryAgain = () => {
-//     setGuess("");
-//     setButton1("");
-//     setFeedback("");
-//   };
+  setTimeout(() => {
+    setBox1(false);
+    setBox2(false);
+    setBox3(false);
+    setBox4(false);
+    setBox5(false);
+    setBox6(false);
+  }, 1800);
 
-//   const onChange = (event) => {
-//     event.preventDefault();
-//     let input = event.target.value.toLowerCase();
-//     setGuess(input);
-//   };
+  // const matchCheck = () => {
+  //   if the first two selected boxes are the same, keep front face up.
 
-  //   const onChange = (event) => {
-  //   let input = event.target.value.toLowerCase();
-  //   setUserInput(input);
-  // };
+  // }
 
   return (
     <>
-    <div className="container">
-    {/* { here is some statement that can be TRUE or FALSE ? if it is TRUE then do this thing : if it is FALSE then do this thing} */}
-    <div className="col1" onClick={() => setBox1(!box1)}>{box1 === true ? 'Cat here!' : ''}</div> 
-    <div className="col2" onClick={() => setBox2(!box2)}>{box2 === true ? 'Snail here!' : ''}</div>
-    <div className="col3" onClick={() => setBox3(!box3)}>{box3 === true ? 'Frog here!' : ''}</div>
-    </div>
+      <div className="boxContainer">
+        {/* { here is some statement that can be TRUE or FALSE ? if it is TRUE then do this thing : if it is FALSE then do this thing} */}
+        <div
+          className={box1 === true ? "box1 front" : "box1 back"}
+          onClick={() => setBox1(!box1)}
+        >
+          {box1 === true ? "Cat here!" : "?"}
+        </div>
+        <div
+          className={box2 === true ? "box2 front" : "box2 back"}
+          onClick={() => setBox2(!box2)}
+        >
+          {box2 === true ? "mouse here" : "?"}
+        </div>
+        <div
+          className={box3 === true ? "box3 front" : "box3 back"}
+          onClick={() => setBox3(!box3)}
+        >
+          {box3 === true ? "frog here!" : "?"}
+        </div>
+        <div
+          className={box4 === true ? "box4 front" : "box4 back"}
+          onClick={() => setBox4(!box4)}
+        >
+          {box4 === true ? "Cat here!" : "?"}
+        </div>
+        <div
+          className={box5 === true ? "box5 front" : "box5 back"}
+          onClick={() => setBox5(!box5)}
+        >
+          {box5 === true ? "mouse here" : "?"}
+        </div>
+        <div
+          className={box6 === true ? "box6 front" : "box6 back"}
+          onClick={() => setBox6(!box6)}
+        >
+          {box6 === true ? "frog here!" : "?"}
+        </div>
+      </div>
     </>
   );
 };
-export default Level3
+export default Level3;
